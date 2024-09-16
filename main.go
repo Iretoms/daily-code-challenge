4ff0ef592b4061d27ac951434b761999174442eb
Iretoms <iretomiwajonathan@gmail.com>
2024-09-16 21:57:38 +0100

    14.longest common prefix

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
 func main() {
 	// fmt.Println(easy.TwoSum([]int{1, 2, 3, 5}, 8))
 	// fmt.Println(easy.IsPalindrome(223))
-	fmt.Println(easy.RomanToInt("XXVI"))
-	fmt.Println(easy.RomanToInt("XX"))
-	fmt.Println(easy.RomanToInt("IV"))
-	fmt.Println(easy.RomanToInt("MCMXCIV"))
+	// fmt.Println(easy.RomanToInt("XXVI"))
+	fmt.Println(easy.LongestCommonPrefix([]string{"flower","flow","flight"}))
 }
