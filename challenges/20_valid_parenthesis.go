3e03a78498603b619c6c065dbe7197ac34d7e7b8
Iretoms <iretomiwajonathan@gmail.com>
2024-09-17 23:49:13 +0100

    20.valid parenthesis

---
+++ challenges/20_valid_parenthesis.go
@@ -0,0 +1,68 @@
+package challenges
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+
+// Input: s = "()"
+
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+
+// Output: false
+
+// Example 4:
+
+// Input: s = "([])"
+
+// Output: true
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+func IsValid(s string) bool {
+	var stack []rune
+
+	for _, char := range s {
+		switch char {
+		case '(', '[', '{':
+			stack = append(stack, char)
+		case ')':
+			if len(stack) == 0 || stack[len(stack)-1] != '(' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case ']':
+			if len(stack) == 0 || stack[len(stack)-1] != '[' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		case '}':
+			if len(stack) == 0 || stack[len(stack)-1] != '{' {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		default:
+			return false
+		}
+	}
+
+	return len(stack) == 0
+}
--- main.go
+++ main.go
@@ -10,5 +10,6 @@ func main() {
 	// fmt.Println(easy.TwoSum([]int{1, 2, 3, 5}, 8))
 	// fmt.Println(easy.IsPalindrome(223))
 	// fmt.Println(easy.RomanToInt("XXVI"))
-	fmt.Println(easy.LongestCommonPrefix([]string{"flower","flow","flight"}))
+	// fmt.Println(easy.LongestCommonPrefix([]string{"flower","flow","flight"}))
+	fmt.Println(easy.IsValid("([])"))
 }
