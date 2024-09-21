e101e9043a3eea55c7e55c831072608843363a00
Iretoms <iretomiwajonathan@gmail.com>
2024-09-21 21:43:46 +0100

    21.merge two sorted lists

--- challenges/21_merge_two_sorted_lists.go
+++ challenges/21_merge_two_sorted_lists.go
@@ -6,11 +6,27 @@ type ListNode struct {
 }
 
 func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	return &ListNode{}
+	
+    head := &ListNode{}
+    current := head
+
+    for list1 != nil && list2 != nil {
+        if list1.Val <= list2.Val {
+            current.Next = list1  
+            list1 = list1.Next   
+        } else {
+            current.Next = list2  
+            list2 = list2.Next    
+        }
+        current = current.Next  
+    }
+
+    if list1 != nil {
+        current.Next = list1
+    } else {
+        current.Next = list2
+    }
+
+    return head.Next
 }
 
-
-
-//client_id= 7677541169700.7755858686372
-//client_secret= ec30498d12de307a7ae4780a7df9f1f6
-//https://hngapprentice-5y64301.slack.com/oauth?client_id=3342870781.7533958452320&scope=chat%3Awrite%2Cchat%3Awrite.public%2Cchat%3Awrite.customize%2Cchannels%3Aread%2Cgroups%3Aread%2Cim%3Aread%2Cmpim%3Aread%2Cusers%3Aread&user_scope=&redirect_uri=https%3A%2F%2Fwww.integromat.com%2Foauth%2Fcb%2Fslack3&state=9f87c96e52fed1a556839365&granular_bot_scope=1&single_channel=0&install_redirect=&tracked=1&response_type=code
